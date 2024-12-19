aa71e535f36bca069e5a95ceb33b9cb7fbbda8a5
sookyeong <sookyeong0505@gmail.com>
2024-12-19 18:12:36 +0900

    책 클래스 구현

--- library/src/main/java/com/kws/Book.java
+++ library/src/main/java/com/kws/Book.java
@@ -15,7 +15,47 @@ public class Book {
         this.year = year;
     }
 
+    public int getBookId() {
+        return bookId;
+    }
 
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String toString() {
+        return "책번호=" + bookId + ", 제목=" + title + ", 글쓴이=" + author + ", 출판사=" + publisher + ", 출판년도=" + year + "]";
+    }
 }
